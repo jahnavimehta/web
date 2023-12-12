9691171fc936844b5a450bce6f5b626395722dd3
Jahnavi Mehta <97538596+jahnavimehta@users.noreply.github.com>
2023-12-12 19:20:53 +0530

    Create DOM Events.js

---
+++ JS 10/DOM Events.js
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button onclick="console.log('button was clicked')">Click Me!</button>
+    <script src="app.js"></script>
+</body>
+</html>
+/************************************************************************/
+  //printing 2 lines on clicking btn
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button 
+    onclick="console.log('button was clicked'); console.log('2nd line to be printed')";
+    >Click Me!</button>
+    <script src="app.js"></script>
+</body>
+</html>
+/*********************************************************************/
+//make multiple btns like on instagram post
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button onclick="console.log('hide post')";>hide post!</button>
+    <button onclick="console.log('share post')";>share post!</button>
+    <button onclick="console.log('like post')";>like post!</button>
+    <button onclick="console.log('save post')";>save post!</button>
+    <script src="app.js"></script>
+</body>
+</html>
