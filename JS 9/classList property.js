2771eef50188662047bc9f46e3e222b9914b9557
Jahnavi Mehta <97538596+jahnavimehta@users.noreply.github.com>
2023-12-12 15:58:13 +0530

    Update classList property.js

--- JS 9/classList property.js
+++ JS 9/classList property.js
@@ -2,3 +2,11 @@ let img = document.querySelector('img');
 img.classList //""
 //go to index.html file and add class to image eg class = "abc xyz"
 img.classList //"abc, xyz"
+/*************************************************************************/
+let heading = document.querySelector('h1');
+heading.classList.add("green");
+heading.classList.add("align");
+heading.classList.remove("align");
+heading.setAttribute('class', 'align');
+heading.classList.toggle('green');
+heading.classList.toggle('green');
