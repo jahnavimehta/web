f8e291c99feefa80c8e94af927d0acb2732da8bf
Jahnavi Mehta <97538596+jahnavimehta@users.noreply.github.com>
2023-12-12 09:58:48 +0530

    Update manipulating attributes.js

--- JS 9/manipulating attributes.js
+++ JS 9/manipulating attributes.js
@@ -1,6 +1,12 @@
 let img = document.querySelector('img');
 img
+//setting id 
 img.setAttribute('id', 'spidermanImg');
 img
+//setting src
 img.setAttribute('src', "assets/creation_3.jpeg");
 img
+//setting class
+img.getAttribute('class');
+img.setAttribute('class', 'images');
+img.getAttribute('class');
