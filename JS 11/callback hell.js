d250e37de16aba95e1cf258725da64418aa6e93d
Jahnavi Mehta <97538596+jahnavimehta@users.noreply.github.com>
2023-12-13 15:24:02 +0530

    Update callback hell.js

--- JS 11/callback hell.js
+++ JS 11/callback hell.js
@@ -15,3 +15,18 @@
     h.style.color = "yellow";
  }, 4000);
 /**********************************************************/
+//fails to work asynhronously - function changes color immediately
+ let h = document.querySelector("h1");
+
+ function changeColor(color){
+    h.style.color = color;
+ }
+
+ //after 2s
+ setTimeout(changeColor("green"), 2000);
+
+ //after 3s
+ setTimeout(changeColor("red"), 3000);
+
+  //after 4s
+  setTimeout(changeColor("yellow"), 4000);
