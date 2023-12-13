cf10a092bb94820e7a864eabb1a2de8b3674be6d
Jahnavi Mehta <97538596+jahnavimehta@users.noreply.github.com>
2023-12-13 14:25:22 +0530

    Create callback hell.js

---
+++ JS 11/callback hell.js
@@ -0,0 +1,17 @@
+ let h = document.querySelector("h1");
+
+ //after 2s
+ setTimeout(() => {
+    h.style.color = "red";
+ }, 2000);
+
+ //after 3s
+ setTimeout(() => {
+    h.style.color = "green";
+ }, 3000);
+
+  //after 4s
+  setTimeout(() => {
+    h.style.color = "yellow";
+ }, 4000);
+/**********************************************************/
